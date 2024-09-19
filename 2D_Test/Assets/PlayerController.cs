b0770dca45325e27a0f2d061159f0f936994da04
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-09-19 23:12:43 +0900

    Feat: 0919 수업내용 (타일맵, 2d 애니메이션)

--- 2D_Test/Assets/PlayerController.cs
+++ 2D_Test/Assets/PlayerController.cs
@@ -6,12 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rigid;
     [SerializeField] SpriteRenderer render;
+    [SerializeField] Animator animator;
+
     [SerializeField] float movePower;
     [SerializeField] float maxMoveSpeed;
     [SerializeField] float jumpPower;
     [SerializeField] float maxFallSpeed;
 
+    [SerializeField] bool isGrounded;
+
     private float x;
+
+    private static int idleHash = Animator.StringToHash("Idle");
+    private static int runHash = Animator.StringToHash("Run");
+    private static int jumpHash = Animator.StringToHash("Jump");
+    private static int fallHash = Animator.StringToHash("Fall");
+
+    private int curAniHash;
 
     private void Update()
     {
@@ -21,6 +32,9 @@ public class PlayerController : MonoBehaviour
         {
             Jump();
         }
+
+        GroundCheck();
+        AnimatorPlay();
     }
 
     private void FixedUpdate()
@@ -40,7 +54,12 @@ public class PlayerController : MonoBehaviour
         {
             rigid.velocity = new Vector2(-maxMoveSpeed, rigid.velocity.y);
         }
-        
+
+        if(rigid.velocity.y < -maxFallSpeed)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, -maxFallSpeed);
+        }
+   
         if(x < 0)
         {
             render.flipX = true;
@@ -58,6 +77,52 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
+        if (isGrounded == false)
+            return;
+
         rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+
+    private void GroundCheck()
+    { 
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.2f);
+
+        if(hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void AnimatorPlay()
+    {
+        int checkAniHash;
+
+        if(rigid.velocity.y > 0.01f)
+        {
+            checkAniHash = jumpHash;
+        }
+        else if (rigid.velocity.y < -0.01f)
+        {
+            checkAniHash = fallHash;
+        }
+
+        else if (rigid.velocity.sqrMagnitude < 0.01f)
+        {
+            checkAniHash = idleHash;
+        }
+        else
+        {
+            checkAniHash = runHash;
+        }
+
+        if (curAniHash != checkAniHash)
+        {
+            curAniHash = checkAniHash;
+            animator.Play(curAniHash);
+        }
     }
 }
